adab30b3f779a93eb419e04eb91f03b6d9a966b1
0xletuss <yamiyuhiko@gmail.com>
2025-06-06 22:45:14 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,11 +5,17 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  const res = await fetch('http://127.0.0.1:5000/api/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password })
-  });
+  const res = await fetch("https://landahan-5.onrender.com/api/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    email: email,
+    password: password
+  })
+})
+
 
   const data = await res.json();
   document.getElementById('message').textContent = data.message;
