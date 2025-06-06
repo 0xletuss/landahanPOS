5f9b77e3e26d2f99c6db3997959a162a44e6ac86
0xletuss <yamiyuhiko@gmail.com>
2025-06-06 22:42:08 +0800

    Update register.js

--- js/register.js
+++ js/register.js
@@ -6,7 +6,7 @@ document.getElementById('register').addEventListener('submit', async (e) => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  const res = await fetch('http://127.0.0.1:5000/api/register', {
+  const res = await fetch("https://landahan-5.onrender.com/api/login", {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name, email, password })
